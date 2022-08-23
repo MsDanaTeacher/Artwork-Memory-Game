354a0b78acf10c7e807a73c2079c6ec795ab29d1
Dana Lobell <danalobell@gmail.com>
2022-08-23 13:52:50 -0400

    form updates

--- src/components/GalleryContainer.js
+++ src/components/GalleryContainer.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GalleryForm from './GalleryForm';
+import GalleryTiles from './GalleryTiles';
 
 export default function GalleryContainer() {
+  const [formData, setFormData] = useState({Title: '', Artist: '', Image: ''})
+  const [newObj, setNewObj] = useState({Title: '', Artist: '', Image: ''})
+
+  function handleFormChange(e){
+    setFormData({...formData, [e.target.name]: e.target.value})
+    // setNewObj({...newObj, [e.target.name]: e.target.value})
+  }
+
+  function handleFormSubmit(e){
+    e.preventDefault()
+    setNewObj({...formData, [e.target.name]: e.target.value})
+    setFormData({Title: '', Artist: '', Image: ''})
+    console.log(newObj)
+  }
+
   return (
-    <div className="galleryContainer">GalleryContainer</div>
+    <div className="galleryContainer">
+      <GalleryForm handleFormChange={handleFormChange} handleFormSubmit={handleFormSubmit} formData={formData}/>
+      <GalleryTiles newObj={newObj} />
+    </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
 
 
 export default function NavBar() {
   return (
-    <div>
+<div>
     <div className="navBar">
-      <p className="navTitles">Welcome</p>
-      <p className="navTitles">Level I</p>
-      <p className="navTitles">Level II</p>
-      <p className="navTitles">Gallery of Art</p>
-    </div>
-    <div id="line">
+      {/* <NavLink
+      to="/"
+      exact
+      activeStyle={{
+        background: "pink",
+      }}
+      className = "navTitles"
+     > 
+    Welcome
+      </NavLink> */}
+      <NavLink
+      to="levelone"
+      exact
+      activeStyle={{
+        background: "pink",
+      }}
+      className = "navTitles"
+    > 
+    Level I
+      </NavLink>
+      <NavLink
+      to="leveltwo"
+      exact
+      activeStyle={{
+        background: "#FFDA7A",
+      }}
+      className = "navTitles"
+    > 
+    Level II
+      </NavLink>
+      <NavLink
+      to="gallery"
+      exact
+      activeStyle={{
+        background: "aquamarine",
+      }}
+      className = "navTitles"
+    > 
+    Gallery of Art
+      </NavLink>
+      </div>
+      <div id="line">
       <p>_____________________________________________________________________________________________________________________________________________________________________________</p>
-    </div>
-
-    </div>
+      </div>
+  </div>
+    
+    )
+  }
+    // {/* <div className="navBar">
+    //   <p className="navTitles">Welcome</p>
+    //   <p className="navTitles">Level I</p>
+    //   <p className="navTitles">Level II</p>
+    //   <p className="navTitles">Gallery of Art</p>
+    // </div>
+    // <div id="line">
+    //   <p>_____________________________________________________________________________________________________________________________________________________________________________</p>
+    // </div> */}
   
 
-  )
-}
+  
