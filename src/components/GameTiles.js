bf098d2e9a6b1e1df3d7c9437e803f1db94239c6
Amanda Sakuma <amandasakuma@gmail.com>
2022-08-23 10:07:44 -0400

    testing matching logic

--- src/components/GameTiles.js
+++ src/components/GameTiles.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-export default function GameTiles() {
+export default function GameTiles({art, handleChoice}) {
+const{image} = art
+
+  const handleClick = () => {
+    handleChoice(art)
+  }
+
   return (
-    <div className="gameTiles">GameTiles
-    <img src="" width="200px" height="200px" />
+    <div className="gameTiles">
+      <div>
+        <img 
+          src={image} 
+          onClick={handleClick} 
+          className="front" 
+          width="200px" height="200px" alt="card front"/>
+      
+        <img className="back" style={{tintColor: "#000000"}} />
+      </div>
+
     </div>
   )
 }
--- src/components/LevelOne.js
+++ src/components/LevelOne.js
@@ -1,8 +1,41 @@
 import React from 'react'
 import GameTiles from './GameTiles'
 
-export default function LevelOne() {
+export default function LevelOne({artwork}) {
+
+  
+  
+  
+  const shuffled = artwork.sort(() => 0.5 - Math.random())
+
+  let selected = shuffled.slice(0, 8);
+
+  let duplicate = selected;
+
+  console.log(selected)
+  console.log(duplicate)
+
+  let eachSelectedArtwork = selected.map((art) => {
+    return <div className="gridOne"><img key={art.Id} src={art.Image} width="200px" height="200px"/></div>
+  })
+
+  let mixedSelectedArtwork = eachSelectedArtwork.sort(() => 0.5 - Math.random())
+
+  let eachDuplicateArtwork = duplicate.map((dupe) => {
+    return <div className="gridOne"><img key={dupe.Id} src={dupe.Image} width="200px" height="200px"/></div>
+  })
+
   return (
-    <div className="levelOne">LevelOne</div>
+    <div className="levelOne">
+      {mixedSelectedArtwork}
+      {eachDuplicateArtwork}
+
+      {/* <div className="card-grid">
+        {artwork.map((art) => (
+          <GameTiles key={art.id} art={art} />
+        ))
+      }
+      </div> */}
+    </div>
   )
 }
