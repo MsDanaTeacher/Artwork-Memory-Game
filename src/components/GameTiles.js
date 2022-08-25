98b5e792212548fabf983c742b3b2421c5f7948e
Amanda Sakuma <amandasakuma@gmail.com>
2022-08-25 12:21:10 -0400

    comments added explaining matching logic

--- src/components/GameTiles.js
+++ src/components/GameTiles.js
@@ -6,24 +6,23 @@ export default function GameTiles({art, handleChoice, flipped}) {
   //Selection handler for LevelOne
   const handleClick = () => {
     handleChoice(art)
-
   }
   
   return (
+    //conditional classname applies CSS changes to be 'flipped' 
     <div className="gameTiles">
       <div className={flipped ? "flipped" : ""}>
       <img 
         src={Image}
         className="art-card"
-        
         width="200px" height="200px" 
         alt="art-card" 
         />
         <div 
+        //each image has a blank card that hides the image until it is flipped
         onClick={handleClick}
         className='blank-card'
         alt="blank-card"
-     
         width="200px" height="200px"
         >
         </div>
--- src/components/LevelOne.js
+++ src/components/LevelOne.js
@@ -18,58 +18,85 @@ export default function LevelOne() {
     }) 
   }, [restart])
 
+//Currently working on refactoring how to set the randomize state, but for now, this works
   useEffect(() => {
     if (randomize) artRandomizer()
   }, [randomize])
   
   const artRandomizer = () => {
+    //When the user clicks 'new game', we generate a new array of images
     let shuffledArt = []
     if (artwork.length > 6) {
       shuffledArt.length = 0;
+
+      //levelOneArray shuffles the full array and takes the first 6 images from the mix
       const levelOneArray = artwork.sort(() => Math.random() -0.5).splice(0, 6)
+
+      //shuffledArt duplicates the six images in our new array...
+      ////...then shuffles them up
+      ////...then assigns each image a random id to track
       shuffledArt = [...levelOneArray, ...levelOneArray]
         .sort(() => Math.random() -0.5)
         .map((art) => ({...art, id: Math.random()}))  
+
+        //then we set state for our new shuffled art array...
+        ///...and reset turns to start the game at 0
         setArtwork(shuffledArt)
         setTurns(0)
     }
   }
 
+  //click event handler that allows users to make two selections per turn
   const handleChoice = (art) => {
+
+    //We track the selections in state (above)...
+    //The ternary below says: when the user clicks on first image, selectionOne = true
+    //...if selectionOne = true, then the user can move on to make a second selection...
+    //if selectionOne=false, then the user still needs to make their first selection
     selectionOne ? setSelectionTwo(art) : setSelectionOne(art)
-    console.log(artwork)
+    //when we console.log(artwork), we should see the info for each item clicked
   } 
 
+//function to compare selections and see whether they match
 useEffect(() => {
+  //First we need to grab the state of our selectionOne and selectionTwo
   if (selectionOne && selectionTwo) {
 
+    //if the Ids for selectionOne and selectionTwo, our user has found a matching pair of images
     if (selectionOne.Id === selectionTwo.Id) {
+      //updating the artwork state using the previous selection's state
       setArtwork(prevArt => {
+         /* returns a new array that takes the previous selection and fires off a function for each image
+        If the image id matches the id of the image selected, then we return a new object
+        ...that new object spreads the image's properties and changes the "matched" value from false to true
+        ...then we return the image with the new matched value*/
         return prevArt.map(art => {
           if (art.Id === selectionOne.Id) {
             return {...art, matched: true}
+            //if it's not a match, then we return the image unchanged
           } else {
             return art
           }
         })
       })
-      // alert("match!")
+      console.log("match!")
       resetTurn()
     } else {
       console.log("no match!")
+      //the set timeout flips the cards over after a few seconds
       setTimeout(() => resetTurn(), 1000)
     }
   }
 }, [selectionOne, selectionTwo])
 
-
-
+  //This resets the selection tracker after the user makes two selections
   const resetTurn = () => {
     setSelectionOne(null)
     setSelectionTwo(null)
     setTurns(prevTurns => prevTurns + 1)
   }
 
+  //This resets our states for the game to restart when the user clicks 'new game'
   let handleNewGame = () => {
     setRestart(prev=>!prev)
     setRandomize(prev=>!prev)
@@ -84,8 +111,14 @@ useEffect(() => {
         {artwork.map((art,i) => (
           <GameTiles 
             key={i} 
+            //onClick selection tracker
             handleChoice={handleChoice}
             art={art} 
+           /* Here we outline when we want to 'flip' a card to reveal the image
+            1) after the first selection
+            2) after the second selection
+            3) when an image's 'matched' value = true
+            **the actual flip magic happens in the css... */
             flipped={art === selectionOne || art === selectionTwo || art.matched}
           />
         ))
